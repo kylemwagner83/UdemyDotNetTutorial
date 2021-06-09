29374be89a6f3b7edff70c2b62baf547da021e8a
Kyle Wagner <70072537+kylemwagner83@users.noreply.github.com>
2021-06-09 14:22:57 -0600

    Section 17

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://localhost:5001/api/'
+  apiURL: 'https://localhost:5001/api/',
+  hubUrl: 'https://localhost:5001/hubs/'
 };
 
 /*
